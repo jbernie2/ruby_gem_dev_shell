4564fc88268891271637359d543e585ba295a2ff
John Bernier <john.bernier@stash.com>
2023-12-14 14:17:58 -0500

    Fix github release script. Remove redundant login command

--- scripts/release_to_github.sh
+++ scripts/release_to_github.sh
@@ -4,8 +4,6 @@ set -e
 $(dirname "$0")/assert_arg.sh $0 1 "gemspec file" $1
 $(dirname "$0")/assert_env_var.sh $0 "GITHUB_TOKEN"
 
-gh auth login --hostname github.com
-
 gem_version=$(ruby -e "
   require 'rubygems'
   puts Gem::Specification::load('$1').version
