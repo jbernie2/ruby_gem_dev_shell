abf7ca75b35699e8ab313d7912996f98f0f5d21e
John Bernier <john.bernier@stash.com>
2024-01-10 15:47:19 -0500

    list default template

--- flake.nix
+++ flake.nix
@@ -1,37 +1,18 @@
 {
-  description = "ruby gem development environment";
+  description = "template for ruby gem development";
+  outputs = { self }: {
+    templates = rec {
+      ruby_gem = {
+        path = ./flake_templates;
+        description = "template for ruby gem development";
+        welcomeText = ''
+          Thanks for using ruby gem dev shell.
+          Run the following command to get started:
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    nix-filter.url = "github:numtide/nix-filter";
-    ruby_gem_dev_shell = {
-      url = "github:jbernie2/ruby_gem_dev_shell/main";
-      flake = false;
+          nix --extra-experimental-features 'nix-command flakes' develop
+        '';
+      };
+      default = ruby_gem;
     };
   };
-
-  outputs = { self, nixpkgs, flake-utils, nix-filter, ruby_gem_dev_shell }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      overlays = [
-        (final: prev: {
-          ruby = final.ruby_3_1;
-        })
-      ];
-      pkgs = import nixpkgs { inherit system overlays; };
-
-    in with pkgs;
-      {
-        devShells = rec {
-          default = run;
-          run = ( callPackage 
-            ruby_gem_dev_shell
-            { 
-              project_root = ./.;
-            }
-          );
-        };
-      }
-    );
 }
